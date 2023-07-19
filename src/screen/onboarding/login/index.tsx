49e02cadc33cf983866b49eb86cd924c35dbda73
taniabansal02 <tania.bansal@netsolutions.com>
2023-07-19 20:00:54 +0530

    login

--- src/screen/onboarding/login/index.tsx
+++ src/screen/onboarding/login/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
 import React, { useState } from 'react'
 import fonts from '../../../assets/font';
 import { moderateScale, moderateScaleVertical, textScale } from '../../../components/responsiveSize';
@@ -23,7 +23,7 @@ const Login = () => {
     }
 
     return (
-        <View style={{ flex: 1, backgroundColor: color.WHITE, }}>
+        <ScrollView style={{ flex: 1, backgroundColor: color.WHITE, }}>
             <View style={{ marginHorizontal: moderateScale(16) }}>
                 <TouchableOpacity style={{ justifyContent: 'flex-end', flexDirection: 'row', marginTop: moderateScaleVertical(25) }}>
                     <Text style={{ color: '#E6427A', textAlign: 'center', fontFamily: font.LatoBold, fontSize: textScale(16), fontStyle: 'normal', lineHeight: moderateScaleVertical(22) }}> Skip </Text>
@@ -45,25 +45,29 @@ const Login = () => {
                     floatingText='Password'
                     isMandatory={true}
                     value={password}
-                    setText={passwordChange} />
+                    setText={passwordChange} 
+                    password={true}
+                    />
 
                 <TouchableOpacity style={{ justifyContent: 'flex-end', flexDirection: 'row', marginTop: moderateScaleVertical(25), marginBottom: moderateScaleVertical(80) }}>
-                    <Text style={{ color: '#E6427A', textAlign: 'center', fontFamily: font.LatoBold, fontSize: textScale(16), fontStyle: 'normal', lineHeight: moderateScaleVertical(22) }}> Forgot Password? </Text>
+                    <Text style={{ color: '#E6427A', textAlign: 'center', fontFamily: font.LatoRegular,  fontWeight:"700", fontSize: textScale(16), fontStyle: 'normal', lineHeight: moderateScaleVertical(22) }}> Forgot Password? </Text>
                 </TouchableOpacity>
 
                 <CustomButton label='LOGIN' onPress={passwordChange} />
 
-                <View style={{flexDirection:'row'}}>
-                    <Text>New To Pageant Planet?</Text>
+                <View style={{flexDirection:'row', marginTop: moderateScaleVertical(50), alignSelf:'center'}}>
+                    <Text style={{color: '#262425', fontFamily: font.LatoRegular,  fontWeight:"500", fontSize: textScale(16), fontStyle:'normal', lineHeight: moderateScaleVertical(22)}}>New To Pageant Planet?</Text>
                     <TouchableOpacity>
-                    <Text style={{ color: '#E6427A', textAlign: 'center', fontFamily: font.LatoBold, fontSize: textScale(16), fontStyle: 'normal', lineHeight: moderateScaleVertical(22) }}> Sign up </Text>
+                    <Text style={{ color: '#E6427A',  fontFamily: font.LatoRegular, fontWeight:"700",fontSize: textScale(16), fontStyle: 'normal', lineHeight: moderateScaleVertical(22) }}> Sign up </Text>
                 </TouchableOpacity>
                 </View>
+
+                <View style={{height: moderateScaleVertical(5), width: moderateScale(150), backgroundColor:'#000', borderRadius: 5, marginTop: moderateScaleVertical(35), alignSelf:'center'}}></View>
 
             </View>
 
 
-        </View>
+        </ScrollView>
     )
 }
 
